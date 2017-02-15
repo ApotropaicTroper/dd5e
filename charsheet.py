c09d20279f41e78573ed08c709943881b5dff5f5
ApotropaicTroper <saiixpuppyvii@gmail.com>
2017-02-15 11:57:45 -0500

    successfully reads form fields

--- charsheet.py
+++ charsheet.py
@@ -1,10 +1,24 @@
-import glob
+import sys, glob
 import PyPDF2
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 char = 'Jebeddo the Green'
 files = glob.glob('C:\\Users\\Jethro\\Desktop\\*.pdf')
 charsheet = -1
+
 for file in files:
 	if char in file:
-		charsheet = PyPDF2.PdfFileReader(open(file,'rb')).getPage(2)
-		print charsheet.extractText()
-#		charsheet.close()
+		charsheet = open(file, 'rb')
+		fileReader = PyPDF2.PdfFileReader(charsheet)
+		headers = fileReader.getFormTextFields()
+		for head in headers:
+			print head
+		print '>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<'
+		fields = fileReader.getFields()
+
+		for key in fields.keys():
+			print key
+			print '    ', fields[key]
+
+		charsheet.close()
