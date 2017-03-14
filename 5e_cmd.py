2c511c4f3c6a21a6cae25f87b55c38032481abcc
ApotropaicTroper <saiixpuppyvii@gmail.com>
2017-03-14 11:40:12 -0400

    New compendium site

--- 5e_cmd.py
+++ 5e_cmd.py
@@ -2,18 +2,12 @@ import random as rand
 import sys, re, glob
 import charsheet
 from bs4 import BeautifulSoup
-import urllib, requests
+import urllib, requests, inflect
 
-#Find new compendium to use0
-#'https://www.dnd-spells.com/spell/' + spell(replace space with '-' and remove punctuation
-#   ^some spells end with '-ritual' in the URL
-#'http://engl393-dnd5th.wikia.com/wiki/' + spell
-compendium = 'https://roll20.net/compendium/dnd5e/Spells:'
-
+compendium = 'https://www.dnd-spells.com/spells/'
 #Problematic spells thus far:
 #Anything with an Apostrophe or a Name
 #Bigby's Hand (Arcane Hand)
-#Enlarge/Reduce (Enlarge Reduce)
 #Evard's Black Tentacles (Black Tentacles)
 #Heroes' Feast
 #Hunter's Mark
@@ -25,7 +19,7 @@ compendium = 'https://roll20.net/compendium/dnd5e/Spells:'
 #Tenser's Floating Disk (Floating Disk)
 exit_cmd = {'quit','exit','halt','end','cease','desist','stop',''}
 Ab_Scores = ['Str','Dex','Con','Int','Wis','Chr']
-
+{'1':'st','2':'nd','3':'rd','4':'th','5':'','6':'','7':'','8':'','9':''}
 				#(number of dice)d(die type)
 def roll(cmd):#returns -1 if exit, 1 if normal oparation
 	if 'd' not in cmd:
@@ -53,12 +47,12 @@ def roll(cmd):#returns -1 if exit, 1 if normal oparation
 
 	out = eval(' + '.join(rolls))
 	out = '(' + cmd + '): [' + ' '.join(rolls) + '] = ' + str(out)
-	print out
+	print out,
 	return 1
 
 #Not all spells do damage, such as Comprehend Languages.
 # These will not be implemented except to be described with "info".
-def chromaticorb(mod,adv='n'):
+def chromaticorb(mod):
 	dmg = raw_input('Damage type? Fire/Cold/Lightning/Thunder/Acid/Poison\n')
 	slot = 0
 	while slot < 1 or slot > 9:
@@ -67,21 +61,23 @@ def chromaticorb(mod,adv='n'):
 		except:
 			print 'That must be a number!',
 
-	roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))
-	if 'a' in adv or 'd' in adv:
-		roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))		
-	if 'y' in raw_input('\nHit? ').lower():
+	roll('d20+' + str(char.CastBonus))
+	response = raw_input('\nHit? ')
+	if 'a' in response or 'd' in response:
+		roll('d20+' + str(char.CastBonus))
+	elif 'n' in response:
+		print 'Too Bad!'
+		return -1
+	if 'y' in response or 'y' in raw_input('\nHit? ').lower():
 		roll(str(slot+2) + 'd8')
 		print dmg.capitalize() + ' damage.'
 	else:
 		print 'Too bad!'	
 
-def eldritchblast(mod,adv='n'):
+def eldritchblast(mod):
 	hits = -1
 	for x in range(0,(char.level+1)/6 + 1):
-		roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))
-		if 'a' in adv or 'd' in adv:
-			roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))		
+		roll('d20+' + str(char.CastBonus))
 		print ''
 	while hits < 0:
 		try:
@@ -99,11 +95,15 @@ def eldritchblast(mod,adv='n'):
 		roll('d10')
 		print 'Force damage.'
 
-def firebolt(mod,adv='n'):#'a' for advantage, 'd' for disadvantage
-	roll('d20+' + str(char.Prof) + '+' + str(eval('char.' + mod + 'Mod')))
-	if adv == 'a' or adv == 'd':
-		roll('d20+' + str(char.Prof) + '+' + str(eval('char.' + mod + 'Mod')))		
-	if 'y' in raw_input('\nHit? ').lower():
+def firebolt(mod):#'a' for advantage, 'd' for disadvantage
+	roll('d20+' + str(char.CastBonus))
+	response = raw_input('\nHit? ')
+	if 'a' in response or 'd' in response:
+		roll('d20+' + str(char.CastBonus))
+	elif 'n' in response:
+		print 'Too Bad!'
+		return -1
+	if 'y' in response or 'y' in raw_input('\nHit? ').lower():
 		roll(str((char.level+1)/6 + 1) + 'd10')
 		print 'Fire damage.'
 	else:
@@ -119,9 +119,6 @@ def lightningbolt(mod):
 	roll(str(slot+5) + 'd6')
 	print 'Lightning damage.'
 
-def magearmor(mod):
-	print 'AC becomes ' + str(13 + char.DexMod)
-
 def magicmissile(mod):
 	slot = 0
 	while slot < 1 or slot > 9:
@@ -133,17 +130,21 @@ def magicmissile(mod):
 		roll('d4+1')
 		print 'Force damage.'
 
-def rayoffrost(mod,adv='n'):
-	roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))
-	if 'a' in adv or 'd' in adv:
-		roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))		
-	if 'y' in raw_input('\nHit? ').lower():
+def rayoffrost(mod):
+	roll('d20+' + str(char.CastBonus))
+	response = raw_input('\nHit? ')
+	if 'a' in response or 'd' in response:
+		roll('d20+' + str(char.CastBonus))
+	elif 'n' in response:
+		print 'Too Bad!'
+		return -1
+	if 'y' in response or 'y' in raw_input('\nHit? ').lower():
 		roll(str((char.level+1)/6 + 1) + 'd8')
 		print 'Cold damage.'
 	else:
 		print 'Too bad!'
 
-def scorchingray(mod,adv='n'):
+def scorchingray(mod):
 	slot = 0
 	while slot < 2 or slot > 9:
 		try:
@@ -152,9 +153,7 @@ def scorchingray(mod,adv='n'):
 			print 'That must be a number, 2-9',
 
 	for x in range(0,slot+1):
-		roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))
-		if 'a' in adv or 'd' in adv:
-			roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))		
+		roll('d20+' + str(char.CastBonus))
 		print ''
 	hits = -1
 	while hits < 0:
@@ -172,30 +171,68 @@ def scorchingray(mod,adv='n'):
 		roll('2d6')
 		print 'Fire damage.'	
 
-def witchbolt(mod,adv='n'):
+def witchbolt(mod):
 	slot = 0
 	while slot < 1 or slot > 9:
 		try:
 			slot = int(raw_input('Spell slot level? '))
 		except:
 			print 'That must be a number, 1-9',
-	roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))
-	if 'a' in adv or 'd' in adv:
-		roll('d20 + ' + str(char.Prof) + ' + ' + str(eval('char.' + mod + 'Mod')))		
-	if 'y' in raw_input('\nHit? ').lower():
+	roll('d20+' + str(char.CastBonus))
+	response = raw_input('\nHit? ')
+	if 'a' in response or 'd' in response:
+		roll('d20+' + str(char.CastBonus))
+	elif 'n' in response:
+		print 'Too Bad!'
+		return -1
+	if 'y' in response or 'y' in raw_input('\nHit? ').lower():
 		roll(str(slot) + 'd12')
 		print 'Lightning damage.'
 	else:
 		print 'Too bad!'
-def info(cmd): #receive name of spell only
-	if cmd == '':
+def info(spell): #receive name of spell only
+	if spell == '':
 		raise Exception
-	spell = cmd.replace(' ','%20')
-	url = compendium + spell + '#h-' + spell
-	soup = BeautifulSoup(requests.get(url).text, 'lxml')
-	title = str(soup.title)[7:].split(' | ')[0]
-	info = soup.find('meta',{'name':'description'})['content']
-	print '\n'.join((title,info))
+	soup = BeautifulSoup(requests.get(compendium).text,'lxml')
+	for tr in soup.tbody.findAll('tr'):
+		td = tr.findAll('td')
+		if spell in td[1].a.string.lower().split('('):
+			alphSoup = BeautifulSoup(requests.get(td[1].a.get('href')).text,'lxml')	#"spell"ing with alphabet soup
+			info = alphSoup.find('div',{'class':'col-md-12'})
+			print '\n', info.h1.span.string
+			p = info.findAll('p')[:-1]
+
+			school = p[0].string
+			stats = p[1].findAll('strong')
+			level = stats[0].string.strip()
+			if level == 'Cantrip':
+				print '', school, level
+			else:
+				print '', inflect.engine().ordinal(level) + '-level', school
+			print '\nCasting Time:',stats[1].string, '\nRange:',stats[2].string
+			print 'Components:',stats[3].string, '\nDuration:', stats[4].string
+
+#if casting time is a reaction, then don't print an extra newline; print it after reaction
+			reaction = 'Reaction' in str(p[2])
+
+			for par in re.sub('</?p>','',str(p[2])).split('<br/>'):
+				if reaction:
+					print ' '*3, par.strip() + '\n'
+					reaction = False
+					continue
+				if par.strip() != '':
+					print ' '*7,par.strip()
+
+
+
+
+			if len(p) == 6:
+				for par in re.sub('</?p>','',str(p[3])).split('<br/>'):
+					print ' '*7,par.strip()
+			print '\nClasses:\n ' + ', '.join([a.string for a in p[-1].findAll('a')]) + '\n'
+			break
+
+
 
 char = charsheet.char(raw_input('Name? '))
 while True:
@@ -203,11 +240,11 @@ while True:
 	command = raw_input('> ')
 	command = command.replace('\'','\\\'')
 	if 'info' in command: 	#print spell description
-		try:
-			info(command[5:])
-		except:
-			char.toString()
-			continue
+#		try:
+		info(command[5:])
+#		except:
+#			char.toString()
+#			continue
 		continue
 	if command in exit_cmd:# and 'y' in raw_input('Are you sure? y/n\n').lower():
 		break
@@ -217,7 +254,7 @@ while True:
 	if len(files) > 0:
 		try:
 			mod = char.CastScore
-			eval(command.replace(' ','') + '(\'' + mod + '\',\'' + raw_input('Adv/Dis? ') + '\')')
+			eval(command.replace(' ','') + '(\'' + mod + '\')')
 		except:
 			info(command)
 		continue
