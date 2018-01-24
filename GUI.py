98916272cf4664290e6f5d6dc87720b267967223
ApotropaicTroper <saiixpuppyvii@gmail.com>
2018-01-24 17:59:20 -0500

    reorganizing lines

--- GUI.py
+++ GUI.py
@@ -4,19 +4,26 @@ End goal: D&D spell manager
 '''
 
 
+
 import Tkinter as tk
 
 class Menu(tk.Frame):
 	def boop(self):
+		'''Placeholder, so the buttons do something'''
 		print 'boop'
 
 	def fidgetWidget(self):
-
-
-		self.menu = tk.Canvas(self,height=256+128,width=256+128,bg='#ffffff')
-		self.menu.create_text(5,5,text='Text',anchor=tk.NW)
-		self.menu.grid(rowspan=19,column=4)
+		self.CardSpace = tk.Frame(self)
 		self.DropOptions = tk.Frame(self)
+		self.BottomOptions = tk.Frame(self)
+		self.CardSpace.grid(row=0,column=1)
+		self.DropOptions.grid(row=0,column=0)
+		self.BottomOptions.grid(row=1,columnspan=2)
+		
+		self.Cards = tk.Canvas(self,height=384,width=384,bg='#ffffff')
+		self.Cards.create_text(5,5,text='Text',anchor=tk.NW)
+		self.Cards.grid(in_=self.CardSpace,rowspan=19,column=4)
+
 		self.Level1 = tk.Button(self,text='1', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
 		self.Level2 = tk.Button(self,text='2', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
 		self.Level3 = tk.Button(self,text='3', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
@@ -26,7 +33,6 @@ class Menu(tk.Frame):
 		self.Level7 = tk.Button(self,text='7', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
 		self.Level8 = tk.Button(self,text='8', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
 		self.Level9 = tk.Button(self,text='9', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-
 		self.Level1.grid(in_=self.DropOptions,row=1,rowspan=2,column=0,columnspan=2,pady=2,padx=8)
 		self.Level2.grid(in_=self.DropOptions,row=3,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
 		self.Level3.grid(in_=self.DropOptions,row=5,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
@@ -36,17 +42,13 @@ class Menu(tk.Frame):
 		self.Level7.grid(in_=self.DropOptions,row=13,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
 		self.Level8.grid(in_=self.DropOptions,row=15,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
 		self.Level9.grid(in_=self.DropOptions,row=17,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
-		self.DropOptions.grid(row=1,column=0,rowspan=9)
 
-		self.BottomOptions = tk.Frame(self)
 		self.Compendium = tk.Button(self, text='Compendium',command=self.boop,height=2,width=12)
 		self.loadList = tk.Button(self, text='Load',command=self.boop,height=2, width=5)
 		self.quit = tk.Button(self, text='quit',command=self.quit,height=2,width=5)
-
 		self.Compendium.grid(in_=self.BottomOptions,row=0,column=0)
 		self.loadList.grid(in_=self.BottomOptions,row=0,column=1)
 		self.quit.grid(in_=self.BottomOptions,row=0,column=2)
-		self.BottomOptions.grid(row=19,columnspan=10)
 
 
 
@@ -63,4 +65,3 @@ root = tk.Tk()
 #bottomframe.pack(side='bottom')
 menu = Menu(master=root)
 menu.mainloop()
-root.destroy()
