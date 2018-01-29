c279c9ce71f2452fd6bf72d1506d39874694b74a
ApotropaicTroper <saiixpuppyvii@gmail.com>
2018-01-29 15:25:43 -0500

    testing listbox, some objects created via loop

--- GUI.py
+++ GUI.py
@@ -1,54 +1,73 @@
 
 '''
-End goal: D&D spell manager
+Support for drag & drop?
 '''
 
+'''
+ff0000 0
+ff7f00 1
+ffff00 2
+00ff00 3
+00ff7f 4
+00ffff 5
+0000ff 6
+7f00ff 7
+ff00ff 8
+'''
 
 
 import Tkinter as tk
 
 class Menu(tk.Frame):
+
+	slots = []
+	listing = 0
+
 	def boop(self):
 		'''Placeholder, so the buttons do something'''
 		print 'boop'
+
+#	def doSomething(self):
+#		print self.listing.curselection()
+
+	def color(self,num):
+		return '#00bf00'
 
 	def fidgetWidget(self):
 		self.CardSpace = tk.Frame(self)
 		self.DropOptions = tk.Frame(self)
 		self.BottomOptions = tk.Frame(self)
-		self.CardSpace.grid(row=0,column=1)
-		self.DropOptions.grid(row=0,column=0)
-		self.BottomOptions.grid(row=1,columnspan=2)
-		
-		self.Cards = tk.Canvas(self,height=384,width=384,bg='#ffffff')
-		self.Cards.create_text(5,5,text='Text',anchor=tk.NW)
+		self.CardSpace.grid(row=1,column=1)
+		self.DropOptions.grid(row=1,column=0)
+		self.BottomOptions.grid(row=2,columnspan=2)
+
+		cardsWidth=125
+		cardsHeight=75
+		self.Cards = tk.Canvas(self,height=7*cardsHeight,width=5*cardsWidth,bg='#ffffff')
+		self.Cards.create_text((5,5),text='Drag stuff from this area. Things are 7 high by 5 wide.',anchor=tk.NW)
 		self.Cards.grid(in_=self.CardSpace,rowspan=19,column=4)
 
-		self.Level1 = tk.Button(self,text='1', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-		self.Level2 = tk.Button(self,text='2', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-		self.Level3 = tk.Button(self,text='3', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-		self.Level4 = tk.Button(self,text='4', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-		self.Level5 = tk.Button(self,text='5', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-		self.Level6 = tk.Button(self,text='6', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-		self.Level7 = tk.Button(self,text='7', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-		self.Level8 = tk.Button(self,text='8', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-		self.Level9 = tk.Button(self,text='9', command=self.boop, height=2,width=15,bg='#ffffff',fg='#000000')
-		self.Level1.grid(in_=self.DropOptions,row=1,rowspan=2,column=0,columnspan=2,pady=2,padx=8)
-		self.Level2.grid(in_=self.DropOptions,row=3,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
-		self.Level3.grid(in_=self.DropOptions,row=5,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
-		self.Level4.grid(in_=self.DropOptions,row=7,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
-		self.Level5.grid(in_=self.DropOptions,row=9,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
-		self.Level6.grid(in_=self.DropOptions,row=11,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
-		self.Level7.grid(in_=self.DropOptions,row=13,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
-		self.Level8.grid(in_=self.DropOptions,row=15,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
-		self.Level9.grid(in_=self.DropOptions,row=17,rowspan=2,column=0,columnspan=2,pady=2,padx=2)
-
-		self.Compendium = tk.Button(self, text='Compendium',command=self.boop,height=2,width=12)
+		dropWidgetHeight=50
+		dropWidgetWidth=50
+		for level in range(0,10):
+			self.slots.append(tk.Canvas(self,height=dropWidgetHeight,width=dropWidgetWidth,bg=self.color(level)))
+			self.slots[level].create_text((25,25),text=str(level+1),anchor=tk.CENTER,fill='#000000')
+			self.slots[level].grid(in_=self.DropOptions,row=level)
+		self.Compendium = tk.Button(self, text='Compendium',height=2,width=12)
 		self.loadList = tk.Button(self, text='Load',command=self.boop,height=2, width=5)
 		self.quit = tk.Button(self, text='quit',command=self.quit,height=2,width=5)
 		self.Compendium.grid(in_=self.BottomOptions,row=0,column=0)
 		self.loadList.grid(in_=self.BottomOptions,row=0,column=1)
 		self.quit.grid(in_=self.BottomOptions,row=0,column=2)
+
+		listing = tk.Listbox(self)
+		listing.insert(tk.END,'testing')
+		listing.insert(tk.END,'testing')
+		listing.insert(1,'1')
+		listing.insert(2,'2')
+		listing.insert(3,'3')
+		listing.delete(0)
+		listing.grid(row=0)
 
 
 
@@ -65,3 +84,4 @@ root = tk.Tk()
 #bottomframe.pack(side='bottom')
 menu = Menu(master=root)
 menu.mainloop()
+
