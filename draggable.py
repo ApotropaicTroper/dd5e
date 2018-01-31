6950c5db766fc6d73ac23c79f6e9f938d5d16910
ApotropaicTroper <saiixpuppyvii@gmail.com>
2018-01-31 16:34:59 -0500

    method to set widget as draggable, first grab attempt

--- draggable.py
+++ draggable.py
@@ -1,6 +1,6 @@
-import tkinter as tk
+import Tkinter as tk
 
-class draggable():
+class dragManager(object):
 
 	mouseX = 0
 	mouseY = 0	#position at which the widget was grabbed. Updates as it gets dragged around
@@ -8,19 +8,35 @@ class draggable():
 
 
 
-#widget bound to <ButtonPress-1> event calls this method.
-#create a window at cursor position identical to grabbed widget.
-#Bind this to <B1-Motion> event to call onDrag(), <ButtonRelease-1> event to call onDrop() 
-	def onGrab():
-		'''Grab a widget to drag'''
+#Remove window, do something with the widget under it
+	@staticmethod
+	def onDrop(event):
+		'''Release grabbed widget'''
 		pass
 
 #Move window to mouse position (or the same amount as the mouse moved)
-	def onDrag():
+	@staticmethod
+	def onDrag(event):
 		'''Move grabbed widget'''
 		pass
 
-#Remove window, do something with the widget under it
-	def onDrop():
-		'''Release grabbed widget'''
-		pass
+#widget bound to <ButtonPress-1> event calls this method.
+#create a window at cursor position, identical to grabbed widget.
+#Bind this to <B1-Motion> event to call onDrag(), <ButtonRelease-1> event to call onDrop() 
+	@staticmethod
+	def onGrab(event):
+		'''Grab a widget to drag'''
+		widgetConfig = event.widget.config()
+		dragFrame = tk.Frame(tk.Tk())
+		dragFrame.grid()
+		event.widget.grid(in_=dragFrame)
+		print 'Grabbed at',event.x,event.y
+
+#method for setting a widget as draggable?
+	@staticmethod
+	def canDrag(widget):
+		'''set a given widget to be grabbable'''
+		widget.bind('<ButtonPress-1>',lambda e: dragManager.onGrab(e))
+
+
+#print draggable.onGrab()
