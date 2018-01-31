d654759d16e453aa83c93490f5c8fa53136ec30f
ApotropaicTroper <saiixpuppyvii@gmail.com>
2018-01-31 13:17:36 -0500

    Initial drag/drop attempt

--- GUI.py
+++ GUI.py
@@ -1,6 +1,12 @@
 
 '''
 Support for drag & drop?
+Track left button down, button hold/motion, button release
+<Button-1> (or <ButtonPress-1> or <1>
+ remove object from its section?
+<B1-Motion>
+ set coordinates to that of mouse
+<ButtonRelease-1>
 '''
 
 '''
@@ -18,70 +24,108 @@ ff00ff 8
 
 import Tkinter as tk
 
-class Menu(tk.Frame):
-
-	slots = []
-	listing = 0
-
-	def boop(self):
-		'''Placeholder, so the buttons do something'''
-		print 'boop'
-
-#	def doSomething(self):
-#		print self.listing.curselection()
-
-	def color(self,num):
-		return '#00bf00'
-
-	def fidgetWidget(self):
-		self.CardSpace = tk.Frame(self)
-		self.DropOptions = tk.Frame(self)
-		self.BottomOptions = tk.Frame(self)
-		self.CardSpace.grid(row=1,column=1)
-		self.DropOptions.grid(row=1,column=0)
-		self.BottomOptions.grid(row=2,columnspan=2)
-
-		cardsWidth=125
-		cardsHeight=75
-		self.Cards = tk.Canvas(self,height=7*cardsHeight,width=5*cardsWidth,bg='#ffffff')
-		self.Cards.create_text((5,5),text='Drag stuff from this area. Things are 7 high by 5 wide.',anchor=tk.NW)
-		self.Cards.grid(in_=self.CardSpace,rowspan=19,column=4)
-
-		dropWidgetHeight=50
-		dropWidgetWidth=50
-		for level in range(0,10):
-			self.slots.append(tk.Canvas(self,height=dropWidgetHeight,width=dropWidgetWidth,bg=self.color(level)))
-			self.slots[level].create_text((25,25),text=str(level+1),anchor=tk.CENTER,fill='#000000')
-			self.slots[level].grid(in_=self.DropOptions,row=level)
-		self.Compendium = tk.Button(self, text='Compendium',height=2,width=12)
-		self.loadList = tk.Button(self, text='Load',command=self.boop,height=2, width=5)
-		self.quit = tk.Button(self, text='quit',command=self.quit,height=2,width=5)
-		self.Compendium.grid(in_=self.BottomOptions,row=0,column=0)
-		self.loadList.grid(in_=self.BottomOptions,row=0,column=1)
-		self.quit.grid(in_=self.BottomOptions,row=0,column=2)
-
-		listing = tk.Listbox(self)
-		listing.insert(tk.END,'testing')
-		listing.insert(tk.END,'testing')
-		listing.insert(1,'1')
-		listing.insert(2,'2')
-		listing.insert(3,'3')
-		listing.delete(0)
-		listing.grid(row=0)
-
-
-
-	def __init__(self, master=None):
-		tk.Frame.__init__(self, master)
-		self.grid()
-		self.fidgetWidget()
-
+#class Menu(tk.Frame):
 
 root = tk.Tk()
+slots = []
+listing = 0
+dragframe = None
+mousex = 0
+mousey = 0
+
+def boop(self):
+	'''Placeholder, so the buttons do something'''
+	print 'boop'
+
+def react(event):
+	print event.x, event.y
+#def doSomething(self):
+#	print self.listing.curselection()
+
+def color(num):
+	return '#00bf00'
+
+def onGrab(event):
+	global mousex
+	global mousey
+	print mousex, mousey, '->',
+	mousex = event.x
+	mousey = event.y
+	print mousex, mousey
+	dragFrame = tk.Frame(tk.Tk())
+
+	dragFrame.place()
+#	target.config(in_=dragFrame)
+	# create new window containing a single widget
+
+
+def onDrag(event,target):
+	pass
+
+def fidgetWidget(master=None):
+
+	dropWidgetHeight=50
+	dropWidgetWidth=50
+
+	for level in range(0,10):
+		slots.append(tk.LabelFrame(text=str(level),height=dropWidgetHeight,width=dropWidgetWidth))
+		slots[level].grid(row=level)
+		slots[level].bind('<ButtonPress-1>',onGrab)
+
+#	last = 
+
+
+	'''
+	listing = tk.Listbox(self)
+	listing.insert(tk.END,'testing')
+	listing.insert(tk.END,'testing')
+	listing.insert(1,'1')
+	listing.insert(2,'2')
+	listing.insert(3,'3')
+	listing.delete(0)
+	listing.grid(row=20,columnspan=2)
+
+	self.CardSpace = tk.Frame(self)
+	self.DropOptions = tk.Frame(self)
+	self.BottomOptions = tk.Frame(self)
+	self.CardSpace.grid(row=1,column=1)
+	self.DropOptions.grid(row=1,column=0)
+	self.BottomOptions.grid(row=2,columnspan=2)
+
+	cardsWidth=125
+	cardsHeight=75
+ 	self.Card = tk.Canvas(self,height=7*cardsHeight,width=5*cardsWidth,bg='#ffffff')
+ 	self.Card.create_text((5,5),text='Drag stuff from this area. Things are 7 high by 5 wide.',anchor=tk.NW)
+
+	self.Card.bind('<Button-1>',self.react)
+	self.Card.grid(in_=self.CardSpace,rowspan=19,column=4)
+
+	self.Compendium = tk.Button(self, text='Compendium',height=2,width=12)
+	self.loadList = tk.Button(self, text='Load',command=self.boop,height=2, width=5)
+	self.Compendium.grid(in_=self.BottomOptions,row=0,column=0)
+	self.loadList.grid(in_=self.BottomOptions,row=0,column=1)
+	'''
+	QUIT = tk.Button(text='quit',command=quit,height=2,width=5)
+	QUIT.grid(row=0,column=2)
+
+
+
+#	def __init__(self, master=None):
+#		main = tk.Frame.__init__(self,master)
+#		tk.Frame.__init__(self,master)
+#		self.overridedirect(1)
+#		self.grid()
+#		self.fidgetWidget()
+
+
+masterFrame = tk.Frame(root)
+#root.overrideredirect(1)
+fidgetWidget(root)
+masterFrame.mainloop()
 #frame = tk.Frame(root)
 #frame.pack()
 #bottomframe = tk.Frame(root)
 #bottomframe.pack(side='bottom')
-menu = Menu(master=root)
-menu.mainloop()
+#menu = Menu(master=root)
+#menu.mainloop()
 
