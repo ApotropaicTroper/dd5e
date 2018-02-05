84180d870d39cc6316ca544e40a88cfed49e8580
ApotropaicTroper <saiixpuppyvii@gmail.com>
2018-02-05 17:02:16 -0500

    Copying features from LabelFrame

--- draggable.py
+++ draggable.py
@@ -5,8 +5,7 @@ class dragManager(object):
 	mouseX = 0
 	mouseY = 0	#position at which the widget was grabbed. Updates as it gets dragged around
 	dragWidget = None #grabbed widget
-
-
+	dragFrame = None #container for widget
 
 #Remove window, do something with the widget under it
 	@staticmethod
@@ -20,21 +19,37 @@ class dragManager(object):
 		'''Move grabbed widget'''
 		pass
 
+	#get widget, get type of widget. Return a copy of the widget
+	#can't pass config() to dragWIdget.config(); error with too many of the parameters
+	@classmethod
+	def copyWidget(cls,widget):
+		copyAttrib = {key:widget[key] for key in {'bd','bg','fg','font','height','width','text'}}
+		print copyAttrib
+		cls.dragWidget = tk.LabelFrame(cls.dragFrame)
+		cls.dragWidget.config(copyAttrib)
+
+
 #widget bound to <ButtonPress-1> event calls this method.
 #create a window at cursor position, identical to grabbed widget.
 #Bind this to <B1-Motion> event to call onDrag(), <ButtonRelease-1> event to call onDrop() 
-	@staticmethod
-	def onGrab(event):
+#changing parent of a widget isn't possible, so make a copy of it
+	@classmethod
+	def onGrab(cls,event):
 		'''Grab a widget to drag'''
-		widgetConfig = event.widget.config()
-		dragFrame = tk.Frame(tk.Tk())
-		dragFrame.grid()
-		event.widget.grid(in_=dragFrame)
-		print 'Grabbed at',event.x,event.y
+		cls.dragFrame = tk.Toplevel()
+
+		cls.copyWidget(event.widget)
+#		print cls.dragCopy['text']
+#		print 'DragFrame:',cls.dragFrame,'\nDragCopy:',cls.dragCopy
+		cls.dragWidget.grid(in_=cls.dragFrame)
+		cls.dragFrame.grid()
+#		dragCopy.grid(in_=dragFrame)
+#		print event.widget.config()
+#		print 'Grabbed at',event.x,event.y
 
 #method for setting a widget as draggable?
-	@staticmethod
-	def canDrag(widget):
+	@classmethod
+	def canDrag(cls,widget):
 		'''set a given widget to be grabbable'''
 		widget.bind('<ButtonPress-1>',lambda e: dragManager.onGrab(e))
 
