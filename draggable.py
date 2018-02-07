86bdee823b3e3cc9934f35586a60532e7fbddc3f
ApotropaicTroper <saiixpuppyvii@gmail.com>
2018-02-06 19:29:00 -0500

    dragframe moves with mouse

--- draggable.py
+++ draggable.py
@@ -2,23 +2,28 @@ import Tkinter as tk
 
 class dragManager(object):
 
-	mouseX = 0
-	mouseY = 0	#position at which the widget was grabbed. Updates as it gets dragged around
+	mouseX = 0 #position in window in which widget was grabbed
+	mouseY = 0	
 	dragWidget = None #grabbed widget
 	dragFrame = None #container for widget
 
 #Remove window, do something with the widget under it
-	@staticmethod
-	def onDrop(event):
+	@classmethod
+	def onDrop(cls,event):
 		'''Release grabbed widget'''
-		pass
+		print 'Release'
 
 #Move window to mouse position (or the same amount as the mouse moved)
-	@staticmethod
-	def onDrag(event):
+	@classmethod
+	def onDrag(cls,event):
 		'''Move grabbed widget'''
-		pass
-
+#		cls.mouseX = cls.dragFrame.winfo_pointerx()
+#		cls.mouseY = cls.dragFrame.winfo_pointery()
+#		print cls.mouseX,cls.mouseY
+		print cls.dragFrame.winfo_width(),cls.dragFrame.winfo_height(),cls.dragFrame.winfo_x(),cls.dragFrame.winfo_y()
+		cls.dragFrame.geometry('%dx%d%+d%+d' % (cls.dragFrame.winfo_width(),cls.dragFrame.winfo_height(),
+												event.x_root-cls.mouseX,event.y_root-cls.mouseY))
+#		print cls.mouseX, cls.mouseY
 
 	#get widget, get type of widget. Return a copy of the widget
 	#can't pass config() to dragWIdget.config(); error with too many of the parameters
@@ -26,12 +31,11 @@ class dragManager(object):
 	def copyWidget(cls,widget):
 
 		copyAttrib = {key:widget[key] for key in {'bd','bg','fg','font','height','width','text'}}
-		print copyAttrib
 		cls.dragWidget = tk.LabelFrame(cls.dragFrame)
 		cls.dragWidget.config(copyAttrib)
 
 
-#widget bound to <ButtonPress-1> event calls this method.
+
 #create a window at cursor position, identical to grabbed widget.
 #Bind this to <B1-Motion> event to call onDrag(), <ButtonRelease-1> event to call onDrop() 
 #changing parent of a widget isn't possible, so make a copy of it
@@ -43,15 +47,20 @@ class dragManager(object):
 		cls.copyWidget(event.widget)
 		cls.dragWidget.grid(in_=cls.dragFrame)
 		cls.dragFrame.geometry('%dx%d%+d%+d' % (cls.dragWidget.winfo_reqwidth(),cls.dragWidget.winfo_reqheight(),
-													event.widget.winfo_pointerx(),event.widget.winfo_pointery()))
+													event.widget.winfo_pointerx()-event.x,event.widget.winfo_pointery()-event.y))
+		cls.mouseX = event.x
+		cls.mouseY = event.y
 		cls.dragFrame.overrideredirect(1)
 		cls.dragFrame.grid()
+		cls.dragFrame.bind('<B1-Motion>',lambda e: cls.onDrag(e))
+		cls.dragFrame.bind('<ButtonRelease-1>', lambda e: cls.onDrop(e))
+
 
 #method for setting a widget as draggable?
 	@classmethod
 	def canDrag(cls,widget):
 		'''set a given widget to be grabbable'''
-		widget.bind('<ButtonPress-1>',lambda e: dragManager.onGrab(e))
+		widget.bind('<ButtonPress-1>',lambda e: cls.onGrab(e))
 
 
 #print draggable.onGrab()
